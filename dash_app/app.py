1afecc973720f26e0321141f792e75935825a593
CarlosENMorais <216812100+CarlosENMorais@users.noreply.github.com>
2025-06-27 06:57:58 -0300

    Update app.py
    configurando portas para o deploy

--- dash_app/app.py
+++ dash_app/app.py
@@ -28,4 +28,5 @@ registrar_callbacks(app,
                     niveis_obesidade)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port, debug=True)
